05fbcda968d89f43566c9a751a9657bb00f313fa
Parker Duncan <100903172+ParkerDuncanDev@users.noreply.github.com>
2022-11-14 09:56:12 -0600

    Add files via upload

---
+++ Section Two/Bus.java
@@ -0,0 +1,17 @@
+public class Bus {
+    
+    public static void main(String[] args) {
+        String sentence = "Number of passengers: ";
+        int passengers = 0;
+
+        System.out.println(sentence + passengers);
+        passengers += 9;
+        System.out.println(sentence + passengers);
+        passengers -= 5;
+        System.out.println(sentence + passengers);
+        passengers -= 4;
+        System.out.println(sentence + passengers);
+
+        System.out.println("End of shift");
+    }
+}
---
+++ Section Two/WholeNumbers.java
@@ -0,0 +1,11 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        int number = 1;
+        while (number < 1000000000) {
+            System.out.println(number);
+             number = number * 2;
+        }
+        
+
+    }
+}
