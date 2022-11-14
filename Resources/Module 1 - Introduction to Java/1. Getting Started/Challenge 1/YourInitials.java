d4c6f66907348e349f17431f21c0d9ccaca2f5f8
parkerduncandev <parkerduncan1009@gmail.com>
2022-11-14 10:02:13 -0600

    java bootcamp initial commit

---
+++ Resources/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         System.out.println("JJJJJ  PPPP   DDDD");
+         System.out.println("  J    P   P  D   D");
+         System.out.println("  J    P   P  D   D");
+         System.out.println("  J    PPPP   D   D");
+         System.out.println("  J    P      D   D");
+         System.out.println("J J    P      D   D");
+         System.out.println(" JJ    P      DDDD");
+
+    }
+}
---
+++ Resources/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -0,0 +1,31 @@
+public class Biography {
+    public static void main(String[] args) {
+        
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.2 to access the link).
+
+
+        // name (text)
+        String name = "Parker";
+        // age (whole number)
+        int age = 26;
+        // country (text)
+        String country = "The USA";
+        // sport (text)
+        String game = "Red Dead Online";
+        // hours (whole number)
+        int hours = 2;
+        // game (text)
+        String otherGame = "No Mans Sky";
+        // subject (text)
+        String subject = "science";
+        // grade (character)
+        char grade = 'A';
+     
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+        System.out.println("My favourite Game is " + game + ". I play for " + hours + " hours a day");
+        System.out.println("When I'm tired, I like to play " + otherGame + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored an " + grade +".");
+
+        // Compare your result to what's on Learn the Part.
+    }
+}
